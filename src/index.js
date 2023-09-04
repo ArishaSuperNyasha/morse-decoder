c5e2ea0419f9a624ae7c4e6b3400eb7fe687df76
ArishaSuperNyasha <evangelionyouthresort@gmail.com>
2023-09-04 18:05:07 +0500

    feat: write function Mon, Sep  4, 2023  6:05:07 PM

--- src/index.js
+++ src/index.js
@@ -38,7 +38,59 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let arr = []
+
+    function splitBy10 (start = 0) {
+
+        if (expr.length <= start) return;
+
+        let end = start + 10;
+        arr.push(expr.slice(start, end));
+        splitBy10(end);
+    }
+
+    splitBy10();
+
+    let result = [];
+
+    for (let item of arr) {
+
+        let letter = '';
+        let first1 = false;
+        let stack = [];
+
+        for (let i = 0; i < item.length; i++) {
+
+            if (first1 === false && (item[i] === '0' || item[i] === '*')) {}
+            else if (item[i] === '1') {
+
+                if (first1 === false) first1 = true;
+
+                if (stack[stack.length - 1] === '1') {
+                    stack.pop();
+                    letter += '-';
+                }
+                else {
+                    stack.push('1');
+                }
+            }
+            else if (item[i] === '0') {
+
+                if (stack[stack.length - 1] === '1') {
+                    stack.pop();
+                    letter += '.';
+                }
+            }
+        }
+
+        if (letter.length < 1) result.push(' ');
+        else {
+            result.push(MORSE_TABLE[letter]);
+        }
+    }
+
+    return result.join('');
 }
 
 module.exports = {
